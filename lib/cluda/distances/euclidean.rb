79a41e63cc2f7d2a08251d8ec3ed10024330dc6d
Enrique Figuerola <enrique.figuerola@mydrivesolutions.com>
2014-01-27 15:03:14 +0000

    Addeed euclidean distance

---
+++ lib/cluda/distances/euclidean.rb
@@ -0,0 +1,10 @@
+module Cluda
+  class Euclidean <  Distance
+    
+    def self.distance(x0, x)
+      validate( [x0,x] )
+
+      sqrt( (x0[:x] - x[:x]) ** 2 + (x0[:y] - x[:y]) ** 2 )
+    end
+  end
+end
